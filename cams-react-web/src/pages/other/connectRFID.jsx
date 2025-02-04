5258925c8ae71fab0361c9699f5b4d6686fc5874
Danteh Cheung <dantehcheung@gmail.com>
2025-02-05 01:19:52 +0800

    disconnect and layout change to get andy js

--- cams-react-web/src/pages/other/connectRFID.jsx
+++ cams-react-web/src/pages/other/connectRFID.jsx
@@ -1,91 +1,147 @@
-import React from "react";
-import { Typography, Select, Button, Divider, List, Form, Row, Col, Card } from "antd";
+import React, { useState, useEffect } from "react";
+import { Typography, Select, Button, Divider, Form, Row, Col, Card, message } from "antd";
 import RFID from "../../components/commonRFID/RFID";
+const { ipcRenderer } = window.require("electron");
+let isLoad = false;
 
 const { Title } = Typography;
-const data = ["test", "test.", "test", "test.", "test"];
-
-
-
 
 const ConnectRFID = () => {
+  const [data, setData] = useState("Item1");
+  const [availableDevices, setAvailableDevices] = useState([]);
+
+  const showDebugMsg = (e) => {
+    ipcRenderer.on(e, (event, message) => {
+      document.getElementById("output").innerHTML += `<span style="color:red;">${e}</span>: ${message}<br>`;
+    });
+  };
+
+  const addIpcRendererOn = () => {
+    ipcRenderer.on("replyGetUsbDeviceList", (event, devices) => {
+      populateAvailableDevices(devices);
+    });
+
+    showDebugMsg("replyConnectUsbRfidReader");
+    showDebugMsg("replySingleRead");
+    showDebugMsg("newScannedTag");
+    showDebugMsg("scanningOver");
+    showDebugMsg("replyGetRfidReaderInformation");
+    showDebugMsg("replyStartLoopRead");
+    showDebugMsg("replyStopLoopRead");
+    showDebugMsg("replyDisconnectUsbRfidReader");
+  };
+
+  const populateAvailableDevices = (j) => {
+    let devices = JSON.parse(j).devices;
+    const deviceOptions = devices.map((device, index) => ({
+      value: index.toString(),
+      label: device,
+    }));
+    setAvailableDevices(deviceOptions);
+  };
+
+  useEffect(() => {
+    if (isLoad) {
+      return;
+    }
+    isLoad = true;
+    addIpcRendererOn();
+    ipcRenderer.send("getUsbDeviceList");
+
+    document.getElementById("clear").addEventListener("click", () => {
+      document.getElementById("output").innerHTML = "";
+    });
+
+    document.getElementById("connectBtn").addEventListener("click", () => {
+      document.getElementById("output").innerHTML += `<span style="color:red;">"connectBtn"</span>: 1<br>`;
+      ipcRenderer.send("connectUsbRfidReader", 0);
+    });
+
+    document.getElementById("execute").addEventListener("click", () => {
+      ipcRenderer.send("startLoopRead", 0);
+    });
+
+    document.getElementById("stop").addEventListener("click", () => {
+      ipcRenderer.send("stopLoopRead", 0);
+    });
+
+    document.getElementById("disconnect").addEventListener("click", () => {
+      document.getElementById("output").innerHTML += `<span style="color:red;">"Disconnected"</span>: 1<br>`;
+      ipcRenderer.send("disconnectUsbRfidReader", 0);
+    });
+  }, []);
+
   return (
     <>
-    <div style={{ padding: 16 }}>
-
-      <Card>
-        <Title level={2}>Connect to RFID Reader</Title>
-        <Form layout="vertical">
-          <Row gutter={[16, 16]}>
-            <Col xs={24} md={12}>
-              <Form.Item label="Connection Type">
-                <Select
-                  showSearch
-                  style={{ width: "100%" }}
-                  placeholder="Search to Select"
-                  optionFilterProp="label"
-                  filterSort={(optionA, optionB) =>
-                    (optionA?.label ?? "").toLowerCase()
-                      .localeCompare((optionB?.label ?? "").toLowerCase())
-                  }
-                  options={[
-                    { value: "1", label: "USB" },
-                    { value: "2", label: "Ethernet" },
-                  ]}
-                />
-              </Form.Item>
-            </Col>
-            <Col xs={24} md={12}>
-              <Form.Item label="Avaliable Device">
-                <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ padding: 16 }}>
+        <Card>
+          <Title level={2}>Connect to RFID Reader</Title>
+          <Form layout="vertical">
+            <Row gutter={[16, 16]}>
+              <Col xs={24} md={12}>
+                <Form.Item label="Connection Type">
                   <Select
+                    id="connectType"
                     showSearch
-                    style={{ flex: 1 }}
-                    placeholder="Search to Select"
+                    style={{ width: "100%" }}
+                    placeholder="SELECT"
                     optionFilterProp="label"
                     filterSort={(optionA, optionB) =>
-                      (optionA?.label ?? "").toLowerCase()
-                        .localeCompare((optionB?.label ?? "").toLowerCase())
+                      (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
                     }
-                    options={[
-                      { value: "device1", label: "Device 1" },
-                      { value: "device2", label: "Device 2" },
-                    ]}
+                    options={[{ value: "USB", label: "USB" }]}
                   />
-                  <Button type="primary" style={{ marginLeft: 10 }}>
-                    Connect
-                  </Button>
-                </div>
-              </Form.Item>
-            </Col>
-          </Row>
-          <Row gutter={[16, 16]}>
-            <Col xs={24}>
-              <Button type="primary">Execute</Button>
-              <Button style={{ marginLeft: 10 }}>Clear</Button>
-            </Col>
-          </Row>
-        </Form>
-      </Card>
+                </Form.Item>
+              </Col>
+              <Col xs={24} md={12}>
+                <Form.Item label="Available Device">
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <Select
+                      id="availableDevice"
+                      showSearch
+                      style={{ flex: 1 }}
+                      placeholder="Search to Select"
+                      optionFilterProp="label"
+                      filterSort={(optionA, optionB) =>
+                        (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+                      }
+                      options={availableDevices}
+                    />
+                   
+                  </div>
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={[16, 16]}>
+              <Col xs={24}>
+                <Button type="primary" id="execute">
+                  Execute
+                </Button>
+                <Button type="primary" id="stop" style={{ marginLeft: 10 }} danger>
+                      Stop
+                    </Button>
+                <Button style={{ marginLeft: 10 }} id="clear">
+                  Clear
+                </Button>
+              <div style={{ float: "right" }}>
+                <Button type="primary" id="connectBtn" style={{ marginLeft: 10 }}>
+                      Connect
+                    </Button>
+                    <Button id="disconnect" type="primary" danger style={{ marginLeft: 10 }}>Disconnect</Button>
+                    </div>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
 
-      <Divider />
+        <Divider />
 
-      <Card title="Result">
-        <List
-          size="large"
-          bordered
-          dataSource={data}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
-        />
-        <Button type="primary" danger style={{ marginTop: "20px" }}>
-          Disconnect
-        </Button>
-      </Card>
+        <Card title="Result">
+          <div id="output" style={{ overflow: "hidden" }}></div>
+        </Card>
+      </div>
      
-    </div>
-    <RFID/>
     </>
-
   );
 };
 
