6ca49f405ea1471695ae550298368d24ad1cdd58
kenlau <ku123912399asb@Gmail.com>
2025-02-11 14:04:39 +0800

    fix comment

--- cams-react-web/vite.config.jsx
+++ cams-react-web/vite.config.jsx
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(
     {
-       // 配置 JSX 运行时
+       
        jsxRuntime: 'automatic', 
-       // 让插件处理 .js 文件中的 JSX
+    
        include: [/\.jsx?$/]
     }
   )],
